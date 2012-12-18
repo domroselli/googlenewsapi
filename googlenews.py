50e233490ba046fd971108331719537d21f62b87
domroselli <eazysneazy@gmail.com>
2012-12-17 22:15:40 -0800

    Some touch-up - adding this time

--- googlenews.py
+++ googlenews.py
@@ -6,11 +6,14 @@ import urllib2
 import json
 import sys
 
+# These are placeholders. They may not even matter but Google suggests
+# it
 IP_ADDR = '192.168.0.1'
 REFERER = 'google.com'
 
-gnewstopics = {
-     'headlines'     : 'h',
+# The available TOPICS through the API
+TOPICS = {
+      'headlines'     : 'h',
       'world'         : 'w',
       'business'      : 'b',
       'nation'        : 'n',
@@ -22,29 +25,27 @@ gnewstopics = {
       'health'        : 'm'
 }
 
-class GnewsSearchResults:
+class GoogleNewsResults:
     """
     Wraps the JSON results from the Google News API into a class.
     """
 
-    def __init__(self, json_result):
-        if not json_result:
+    def __init__(self, json):
+        if not json:
             raise Exception("Empty json_object")
 
-        for k, v in json_result.items():
+        for k, v in json.items():
 
-            if k == 'relatedStories' and (k in json_result):
-                self.relatedStories = [RelatedStory(rs) for rs in json_result[k]]
+            if k == 'relatedStories':
+                self.relatedStories = [RelatedStory(rs) for rs in json[k]]
                 continue
 
-            if k == 'image' and (k in json_result):
-                self.image = Image(json_result[k])
+            if k == 'image':
+                self.image = Image(json[k])
+                continue
 
             # skip unknown dicts and lists
-            if (k in json_result) and\
-               type(json_result[k]) is dict or\
-               type(json_result[k]) is list:
-                    continue
+            if type(json[k]) is dict or type(json[k]) is list: continue
 
             # set the key/value for all attributes
             setattr(self, k, v)
@@ -68,24 +69,26 @@ class Image:
             setattr(self, k, v)
 
 
-class GnewsSearch:
+class GoogleNews:
     """
     Provides access to the Google News API
     """
+
+
     def __init__(self):
         self.ipaddr = IP_ADDR
         self.referer = REFERER
         self.url = ('https://ajax.googleapis.com/ajax/services/search/news?' +
                     'v=1.0&userip={0}&rsz=8'.format(self.ipaddr))
 
-
+        # Create functions for each topic
         def make_topic_func(x):
             return lambda : self._call_api(self.url, {'topic':x})
 
-        for k, v in gnewstopics.items():
+        for k, v in TOPICS.items():
             setattr(self, 'get_{}'.format(k), make_topic_func(v))
 
-
+    # Ad-hoc queries
     def get_query(self, query):
         params = {'q': urllib2.quote(query)}
         return self._call_api(self.url, params)
@@ -98,10 +101,6 @@ class GnewsSearch:
         """
         paramlist = ["{}={}".format(k,v) for k, v in params.items()]
         queryurl = '{0}&{1}'.format(url, '&'.join(paramlist))
-        if sys.flags.debug:
-            pprint(queryurl)
-            pprint(url)
-
         try:
             req = urllib2.Request(queryurl, None, {'Referer': REFERER})
             opened_req = urllib2.urlopen(req)
@@ -109,8 +108,6 @@ class GnewsSearch:
 
             # Process the JSON string.
             resp_obj = json.loads(resp)
-            if sys.flags.debug:
-                pprint(resp_obj)
 
             if resp_obj and\
                 ('responseData' in resp_obj) and\
